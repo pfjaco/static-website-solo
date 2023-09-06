ccff7a4111bfa894a5c0f79047c822ce883cf272
Peter Jacobson <pfjaco@umich.edu>
2023-09-06 18:25:38 -0400

    click-stuff

--- insta485generator/__main__.py
+++ insta485generator/__main__.py
@@ -1,10 +1,14 @@
 """Build static HTML site from directory of HTML templates and plain files."""
+import click
+import pathlib
 
-
-def main():
-    """Top level command line interface."""
-    print("Hello world!")
-
+@click.command()
+@click.option('--output','-o', help="Output directory.", is_flag=True)
+@click.option('--verbose','-v', help="Print more output.", metavar="PATH")
+@click.argument("input_dir", nargs=1, type=click.Path(exists=True))
+def main(input_dir,output,verbose):
+    input_dir = pathlib.Path(input_dir)
+    print(f"DEBUG input_dir={input_dir}")
 
 if __name__ == "__main__":
     main()
