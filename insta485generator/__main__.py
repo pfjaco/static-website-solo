0f0ea2f415758115bcdd3c32288ae3e847a141c9
Peter Jacobson <pfjaco@umich.edu>
2023-09-10 14:49:25 -0400

    multiple-json-objects git commit -mmultiple-json-objects

--- insta485generator/__main__.py
+++ insta485generator/__main__.py
@@ -16,42 +16,43 @@ import click
 def main(input_dir, output, verbose):
     """Templated static website generator."""
     file_read = load_json(input_dir)
-    file_url = [x['url'] for x in file_read]
-    file_url = ''.join(file_url)
-    template_dir = [x['template'] for x in file_read]
-    template_dir = ''.join(template_dir)
-    pwd = os.getcwd()
-    load_template_dir = pwd + file_url + input_dir + "/templates/"
-    template_env = jinja2.Environment(
-        loader=jinja2.FileSystemLoader(load_template_dir),
-        autoescape=jinja2.select_autoescape(['html', 'xml']),
-    )
-    template = template_env.get_template(template_dir)
-    html_output = template.render(file_read[0]["context"])
-    if output is not None:
-        output_dir = ''.join(output)
-        output_p = output_dir
-    else:
-        output_dir = input_dir + "/html"
-        output_p = input_dir
-    try:
-        pathlib.Path(pwd + file_url + output_dir).mkdir(parents=True,
-                     exist_ok=False)
-        filename = pathlib.Path(pwd + file_url + output_dir +
-                                "/" + template_dir)
-        with open(filename, "w") as file_open:
-            file_open.write(html_output)
-        if pathlib.Path.exists(pathlib.Path(pwd + file_url
+    for q in range(len(file_read)):
+        file_url = file_read[q]['url']
+        file_url = ''.join(file_url)
+        template_dir = file_read[q]['template']
+        template_dir = ''.join(template_dir)
+        pwd = os.getcwd()
+        load_template_dir = pwd + '/' + input_dir + "/templates/"
+        template_env = jinja2.Environment(
+            loader=jinja2.FileSystemLoader(load_template_dir),
+            autoescape=jinja2.select_autoescape(['html', 'xml']),
+        )
+        template = template_env.get_template(template_dir)
+        html_output = template.render(file_read[q]["context"])
+        if output is not None:
+            output_dir = ''.join(output)
+            output_p = output_dir
+        else:
+            output_dir = input_dir + "/html"
+            output_p = input_dir
+        try:
+            pathlib.Path(pwd + file_url + output_dir).mkdir(parents=True,
+                        exist_ok=False)
+            filename = pathlib.Path(pwd + file_url + output_dir +
+                                    "/" + template_dir)
+            with open(filename, "w") as file_open:
+                file_open.write(html_output)
+            if pathlib.Path.exists(pathlib.Path(pwd + file_url
                                 + input_dir + "/static/")):
-            shutil.copytree(pathlib.Path(pwd + file_url
-                    + input_dir + "/static/css/"),
-                    pathlib.Path(pwd + file_url + output_dir + "/css/"))
+                shutil.copytree(pathlib.Path(pwd + file_url
+                        + input_dir + "/static/css/"),
+                        pathlib.Path(pwd + file_url + output_dir + "/css/"))
+                if verbose:
+                    print(f"Copied {input_dir}/static -> {output_dir}")
             if verbose:
-                print(f"Copied {input_dir}/static -> {output_dir}")
-        if verbose:
-            print(f"Rendered {template_dir} -> {output_dir}/{template_dir}")
-    except FileExistsError:
-        print(f"Error: '{output_p}' already exists")
+                print(f"Rendered {template_dir} -> {output_dir}{file_url}index.html/")
+        except FileExistsError:
+            print(f"Error: '{output_p}' already exists")
 
 
 def load_json(input_dir):
